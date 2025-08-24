a0081d4179da0299718d4dc5c2270daa3d4beb3f
Gubanov07 <sgubanov325@gmail.com>
2025-08-24 17:02:34 +0700

    'Reading_and_parsing_files'

---
+++ src/Parsers.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Parsers;
+
+function readFile(string $filePath): string
+{
+    if (!file_exists($filePath)) {
+        throw new \Exception("File '{$filePath}' does not exist");
+    }
+
+    $content = file_get_contents($filePath);
+    if ($content === false) {
+        throw new \Exception("Failed to read file '{$filePath}'");
+    }
+
+    return $content;
+}
+
+function parseJson(string $content): object
+{
+    $data = json_decode($content);
+    
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        throw new \Exception("Invalid JSON: " . json_last_error_msg());
+    }
+
+    return $data;
+}
+
+function parseFile(string $filePath): object
+{
+    $content = readFile($filePath);
+    $extension = pathinfo($filePath, PATHINFO_EXTENSION);
+    
+    switch ($extension) {
+        case 'json':
+            return parseJson($content);
+        default:
+            throw new \Exception("Unsupported file format: '{$extension}'");
+    }
+}
