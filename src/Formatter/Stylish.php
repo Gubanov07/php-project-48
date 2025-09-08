b2e5d856b40f0229b04133be012693655b54dcd9
Gubanov07 <sgubanov325@gmail.com>
2025-09-08 19:46:26 +0700

    fix 24

--- src/Formatter/Stylish.php
+++ src/Formatter/Stylish.php
@@ -13,20 +13,20 @@ function format(array $diff, int $depth = 0): string
         
         switch ($node['type']) {
             case 'added':
-                $value = stringify($node['value'], $depth);
+                $value = stringify($node['value'], $depth + 1);
                 $lines[] = "{$signIndent}+ {$key}: {$value}";
                 break;
             case 'removed':
-                $value = stringify($node['value'], $depth);
+                $value = stringify($node['value'], $depth + 1);
                 $lines[] = "{$signIndent}- {$key}: {$value}";
                 break;
             case 'unchanged':
-                $value = stringify($node['value'], $depth);
+                $value = stringify($node['value'], $depth + 1);
                 $lines[] = "{$baseIndent}    {$key}: {$value}";
                 break;
             case 'changed':
-                $oldValue = stringify($node['oldValue'], $depth);
-                $newValue = stringify($node['newValue'], $depth);
+                $oldValue = stringify($node['oldValue'], $depth + 1);
+                $newValue = stringify($node['newValue'], $depth + 1);
                 $lines[] = "{$signIndent}- {$key}: {$oldValue}";
                 $lines[] = "{$signIndent}+ {$key}: {$newValue}";
                 break;
@@ -60,14 +60,19 @@ function stringify($value, int $depth): string
     
     if (is_array($value) || is_object($value)) {
         $value = (array) $value;
-        $indent = str_repeat('    ', $depth);
+        $baseIndent = str_repeat('    ', $depth);
         $innerIndent = str_repeat('    ', $depth + 1);
         $lines = [];
         
         foreach ($value as $key => $val) {
             $lines[] = "{$innerIndent}{$key}: " . stringify($val, $depth + 1);
         }
-        return "{\n" . implode("\n", $lines) . "\n{$indent}}";
+        
+        if (empty($lines)) {
+            return "{}";
+        }
+        
+        return "{\n" . implode("\n", $lines) . "\n{$baseIndent}}";
     }
     
     return (string) $value;
