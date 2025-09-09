868d92a3ffc683bb9227c454173fff570d408ec0
Gubanov07 <sgubanov325@gmail.com>
2025-09-09 19:26:36 +0700

    fix 25

--- src/Differ.php
+++ src/Differ.php
@@ -17,7 +17,7 @@ function genDiff(string $filePath1, string $filePath2, string $format = 'stylish
 function buildDiff(array $data1, array $data2): array
 {
     $allKeys = array_unique(array_merge(array_keys($data1), array_keys($data2)));
-    $sortedKeys = array_values(array_sort($allKeys));
+    $sortedKeys = array_sort($allKeys);
 
     return array_reduce($sortedKeys, function ($acc, $key) use ($data1, $data2) {
         $value1 = $data1[$key] ?? null;
@@ -31,10 +31,10 @@ function buildDiff(array $data1, array $data2): array
             return array_merge($acc, [['type' => 'added', 'key' => $key, 'value' => $value2]]);
         }
 
-        $value1 = is_object($value1) ? (array) $value1 : $value1;
-        $value2 = is_object($value2) ? (array) $value2 : $value2;
+        $processedValue1 = is_object($value1) ? (array) $value1 : $value1;
+        $processedValue2 = is_object($value2) ? (array) $value2 : $value2;
 
-        if (is_array($value1) && is_array($value2)) {
+        if (is_array($processedValue1) && is_array($processedValue2)) {
             return array_merge($acc, [[
                 'type' => 'nested',
                 'key' => $key,
@@ -42,17 +42,25 @@ function buildDiff(array $data1, array $data2): array
             ]]);
         }
 
-        if ($value1 === $value2) {
-            return array_merge($acc, [['type' => 'unchanged', 'key' => $key, 'value' => $value1]]);
+        if ($processedValue1 === $processedValue2) {
+            return array_merge($acc, [['type' => 'unchanged', 'key' => $key, 'value' => $processedValue1]]);
         }
 
-        return array_merge($acc, [['type' => 'changed', 'key' => $key, 'oldValue' => $value1, 'newValue' => $value2]]);
+        return array_merge($acc, [['type' => 'changed', 'key' => $key, 'oldValue' => $value1, 'newValue' => $processedValue2]]);
     }, []);
 }
 
 function array_sort(array $array): array
 {
-    $sorted = $array;
-    sort($sorted);
-    return $sorted;
+return array_reduce($array, function ($acc, $item) {
+        $index = array_reduce(array_keys($acc), function ($idx, $i) use ($acc, $item) {
+            return $item < $acc[$i] ? $i : $idx;
+        }, count($acc));
+        
+        return array_merge(
+            array_slice($acc, 0, $index),
+            [$item],
+            array_slice($acc, $index)
+        );
+    }, []);
 }
